42193b8fd20d5b03128ce659c967ffe7232afff0
Nguyễn Hoàng Hiệp <42592909+nightfury97@users.noreply.github.com>
2025-08-10 10:10:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 // Setup server port
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8002;
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
